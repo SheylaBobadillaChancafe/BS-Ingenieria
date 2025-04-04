b777c1bb2f3a719667d78b2ebf377d7411f33e25
Sheyla Bobadilla Chancafe <sheylabobadillac@gmail.com>
2025-04-03 22:54:50 -0500

    Update

---
+++ script.js
@@ -0,0 +1,12 @@
+
+    // Seleccionar el ícono de hamburguesa y el menú
+
+    const menuToggle = document.getElementById('menu-toggle');
+    const menu = document.querySelector('.menu');
+
+    // Añadir un evento para alternar la clase 'show' al hacer clic en el ícono de hamburguesa
+
+    menuToggle.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
+
